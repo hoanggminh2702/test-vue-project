58cc07bc9b603142df842ff40e82deabbd03b422
hoanggminh2702 <43093294+hoanggminh2702@users.noreply.github.com>
2022-04-29 10:22:32 +0700

    vue project

---
+++ src/App.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+import { reactive, ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+const name = ref("Hoàng Minh");
+const rawHTML = ref("<h1>Hoàng Minh</h1>");
+const state = reactive({ count: 1 });
+
+const model = ref("test v-model");
+
+const testDisabled = ref(true);
+
+function handleChange(e: Event) {
+  const target = e.target as HTMLInputElement;
+
+  name.value = target.value;
+
+  rawHTML.value = `<h1>${target.value}</h1>`;
+}
+</script>
+
+<template>
+  <h1>Name:</h1>
+  <div>{{ name }}</div>
+  <input @input="handleChange" :value="name" />
+  <div v-html="rawHTML"></div>
+  <div>State is : {{ state.count }}</div>
+
+  <h1>Model:</h1>
+  <div>{{ model }}</div>
+  <input v-model="model" />
+
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 3002,
+  },
+});
